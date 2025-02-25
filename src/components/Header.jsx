83c3aa64def778b28fdb4c12ec4f731d4aebf351
mrhl082024 <mrhl082024@gmail.com>
2025-02-25 12:03:17 +0100

    sumting sumting

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,36 @@
 import { Outlet, Link } from "react-router-dom";
 import NavBar from "./NavBar";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState([true]);
+  const [error, setError] = useState([null]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(`https://gutendex.com/books`);
+        if (!response.ok) {
+          throw new Error(`HTTP Error. Status ${response.status}`);
+        }
+        const result = await response.json();
+        const _data = result.results;
+        setData(_data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
-      <NavBar />
+      <NavBar data={data} />
     </>
   );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,5 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
-const NavBar = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState([true]);
-  const [error, setError] = useState([null]);
+const NavBar = () => {};
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      setError(null);
-      try {
-        const response = await fetch(`https://gutendex.com/books`);
-        if (!response.ok) {
-          throw new Error(`HTTP Error. Status ${response.status}`);
-        }
-        const result = await response.json();
-        setData(result);
-        console.log(result);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
-
-  return (
-    <>
-      <div id="navbar-card"></div>
-    </>
-  );
-};
 export default NavBar;
