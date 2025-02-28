e8d151c92110f537e9178562a42bda0c5aaff16e
mrhl082024 <mrhl082024@gmail.com>
2025-02-28 14:32:25 +0100

    add prev and next buttons

--- src/components/ShowBooks.jsx
+++ src/components/ShowBooks.jsx
@@ -6,24 +6,29 @@ const ShowBooks = () => {
 
   return (
     <>
-      <div id="showbooks-card">
-        {loading === true ? <p>Loading...</p> : null}
-        {data === null || data === undefined
-          ? null
-          : data.results.map((book) => (
-              <>
-                <div id="showbook-card">
-                  <p>{book.title} </p>
-                  {book.authors.map((author) => (
-                    <p>{author.name} </p>
-                  ))}
-                  {book.summaries.map((sum) => (
-                    <p>{sum} </p>
-                  ))}
-                </div>
-              </>
-            ))}
+      <div>
+        <button id="prev-page"></button>
+        <button id="next-page"></button>
       </div>
+      <section>
+        <div id="showbooks-card">
+          {loading === true ? <p>Loading...</p> : null}
+          {data === null || data === undefined
+            ? null
+            : data.results.map((book) => (
+                <>
+                  <div id="showbook-card">
+                    <p>{book.title} </p>
+                    <img src={book.formats["image/jpeg"]} alt="" />
+                    {console.log(book.formats["image/jpeg"])}
+                    {book.authors.map((author) => (
+                      <p>{author.name} </p>
+                    ))}
+                  </div>
+                </>
+              ))}
+        </div>
+      </section>
     </>
   );
 };
