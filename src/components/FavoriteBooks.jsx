3cd164f9f0dc48e02f978545388fe088758e9246
mrhl082024 <mrhl082024@gmail.com>
2025-03-04 14:36:43 +0100

    ADD favoritebook

--- src/components/FavoriteBooks.jsx
+++ src/components/FavoriteBooks.jsx
@@ -3,6 +3,21 @@ import { Context } from "./ContextWindow";
 
 const FavoriteBooks = () => {
   const { favorite } = useContext(Context);
-  return <>{}</>;
+  return (
+    <>
+      {favorite.map((book) => (
+        <div id="favorite-card">
+          <h1>{book.title} </h1>
+          <img src={book.formats["image/jpeg"]} alt="" />
+          <ul>
+            Authors:
+            {book.authors.map((authors) => (
+              <li>{authors.name}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </>
+  );
 };
 export default FavoriteBooks;
