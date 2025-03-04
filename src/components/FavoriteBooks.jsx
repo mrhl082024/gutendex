08e1ae655f7d4954633eb478aeef20878b84e818
mrhl082024 <mrhl082024@gmail.com>
2025-03-04 14:44:43 +0100

    EDIT favorite book

--- src/components/FavoriteBooks.jsx
+++ src/components/FavoriteBooks.jsx
@@ -1,21 +1,24 @@
 import { useContext } from "react";
 import { Context } from "./ContextWindow";
+import { Link } from "react-router-dom";
 
 const FavoriteBooks = () => {
   const { favorite } = useContext(Context);
   return (
     <>
       {favorite.map((book) => (
-        <div id="favorite-card">
-          <h1>{book.title} </h1>
-          <img src={book.formats["image/jpeg"]} alt="" />
-          <ul>
-            Authors:
-            {book.authors.map((authors) => (
-              <li>{authors.name}</li>
-            ))}
-          </ul>
-        </div>
+        <Link>
+          <div id="favorite-card">
+            <h1>{book.title} </h1>
+            <img src={book.formats["image/jpeg"]} alt="" />
+            <ul>
+              Authors:
+              {book.authors.map((authors) => (
+                <li>{authors.name}</li>
+              ))}
+            </ul>
+          </div>
+        </Link>
       ))}
     </>
   );
